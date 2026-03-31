c5b67e1ebc97104f4b71ae7a4904dde1a87b8e07
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-03-31 22:55:31 +0700

    Add MySQL: praktikum 3 - ExecuteScalar hitung mata kuliah

--- PraktikumSqlServer/PraktikumSqlServer/Form1.cs
+++ PraktikumSqlServer/PraktikumSqlServer/Form1.cs
@@ -49,6 +49,21 @@ namespace PraktikumSqlServer
             catch (Exception ex) { MessageBox.Show(ex.Message); }
         }
 
+        // Praktikum 3
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi(); conn.Open();
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+                cmd = new SqlCommand(query, conn);
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
         
     }
 }
