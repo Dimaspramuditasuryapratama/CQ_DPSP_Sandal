464ff7947d3622a8a3959e6339a0ef0209d32ba2
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2026-03-31 20:46:02 +0700

    Add MySQL: praktikum 1 - open database connection

--- PraktikumMysql/Form1.cs
+++ PraktikumMysql/Form1.cs
@@ -1,20 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace PraktikumMysql
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        MySqlConnection conn;
+        MySqlCommand cmd;
+
+        private void koneksi()
         {
-            InitializeComponent();
+            conn = new MySqlConnection(
+                "Server=127.0.0.1;database=DBAkademiADO;UID=root;Password=;");
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                koneksi();
+                conn.Open();
+                MessageBox.Show("Message", "Koneksi ke Database Berhasil",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Message", ex.Message,
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
+
 }
